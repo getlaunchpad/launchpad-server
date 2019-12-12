6bdea7bc63130d1ba010c11db7bea86361bb52dc
Lucas Stettner <lucas.stettner@gmail.com>
2019-12-11 19:25:08 -0500

    added kubernetes config

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Launchpad Server
+
+> Server still in very early phases of development
+
+### Helpful Commands
+
+##### Pushing code via GitHub Packages
+
+If you need help getting your creds in order check out [this gist](https://gist.github.com/LucasStettner/66b2108d0fd9663f2c09db5556f69d39)
+
+```shell
+# Build docker container
+$ docker build -t  docker.pkg.github.com/getlaunchpad/launchpad-server/launchpad:latest .
+
+# Publish to Github Packages
+$ docker push docker.pkg.github.com/getlaunchpad/launchpad-server/launchpad:latest
+```
--- app/routes.go
+++ app/routes.go
@@ -1,15 +1,22 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/lucasstettner/launchpad-server/app/handlers"
 
 	"github.com/go-chi/chi"
 )
 
 func (s *Server) initializeRoutes() {
+	// These routes are used for kubernetes
+	s.Router.Get("/health", handlers.HealthHandler)
+	s.Router.Get("/readiness", handlers.ReadinessHandler)
+
 	s.Router.Route("/api/v1", func(r chi.Router) {
-		// These routes are used for kubernetes
-		r.Get("/health", handlers.HealthHandler)
-		r.Get("/readiness", handlers.ReadinessHandler)
+		// Example of a route
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
 	})
 }
