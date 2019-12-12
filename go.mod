3a10372c141446c2703ef36e2f682be346c5bd47
Lucas Stettner <lucas.stettner@gmail.com>
2019-12-12 15:08:16 -0500

    add circleci config

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/jinzhu/gorm v1.9.11
 	github.com/joho/godotenv v1.3.0
+	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421
 )
--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/lucasstettner/launchpad-server/app"
 )
 
 var server = app.Server{}
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Fetching env variables")
+	}
+
 	server.Initialize()
 }
