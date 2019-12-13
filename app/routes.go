2d13425ebb52a1f0f58d02dd3871afb345716617
Lucas Stettner <lucas.stettner@gmail.com>
2019-12-12 21:31:03 -0500

    add / route for k8 testing

--- app/routes.go
+++ app/routes.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 
 	"github.com/lucasstettner/launchpad-server/app/handlers"
-
-	"github.com/go-chi/chi"
 )
 
 func (s *Server) initializeRoutes() {
@@ -13,10 +11,7 @@ func (s *Server) initializeRoutes() {
 	s.Router.Get("/health", handlers.HealthHandler)
 	s.Router.Get("/readiness", handlers.ReadinessHandler)
 
-	s.Router.Route("/api/v1", func(r chi.Router) {
-		// Example of a route
-		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-		})
+	s.Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
 	})
 }
