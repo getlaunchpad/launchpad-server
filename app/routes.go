1337eaf57050da5d42dcb1fa6cef5751ffcb0fd4
Lucas Stettner <lucas.stettner@gmail.com>
2019-12-13 20:23:19 -0500

    modify / route

--- app/routes.go
+++ app/routes.go
@@ -1,17 +1,19 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/lucasstettner/launchpad-server/app/handlers"
 )
 
 func (s *Server) initializeRoutes() {
+	s.Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hello world!")
+	})
+
 	// These routes are used for kubernetes
 	s.Router.Get("/health", handlers.HealthHandler)
 	s.Router.Get("/readiness", handlers.ReadinessHandler)
-
-	s.Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	})
 }
